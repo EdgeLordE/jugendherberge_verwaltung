4a5963c06d8a985bd4c637747d915370cf9cf3bc
emil.wagner@student.htl-rankweil.at <emil.wagner@student.htl-rankweil.at>
2024-10-30 20:57:19 +0000

    Edited Startseite

--- client_code/Startseite/__init__.py
+++ client_code/Startseite/__init__.py
@@ -20,8 +20,12 @@ class Startseite(StartseiteTemplate):
     self.current_City = [self.drop_down_City.items[self.drop_down_City.selected_value - 1][0], self.drop_down_City.selected_value]
 
     self.drop_down_MoreUser.items = anvil.server.call('get_More_user', self.current_user[0])
-    self.drop_down_Room.items = anvil.server.call('get_zimmer_for_jugendherberge', self.current_City[1]+1)
+    #self.drop_down_Room.items = anvil.server.call('get_zimmer_for_jugendherberge', self.current_City[1]+1)
 
+    self.start_date_check = False
+    self.end_date_check = False
+
+  
   def drop_down_City_change(self, **event_args):
     """This method is called when an item is selected"""
     self.current_City = [self.drop_down_City.items[self.drop_down_City.selected_value - 1][0], self.drop_down_City.selected_value]
@@ -38,6 +42,24 @@ class Startseite(StartseiteTemplate):
     """This method is called when an item is selected"""
     self.current_pricecategorie = [self.drop_down_PriceCategorie.items[self.drop_down_PriceCategorie.selected_value - 1][0], self.drop_down_PriceCategorie.selected_value]
     print(self.current_pricecategorie)
+
+  def date_picker_StartDate_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    if len(str(self.date_picker_StartDate.date)) == 10:
+      self.start_date_check = True
+    else:
+      self.end_date_check = False
+    print(self.start_date_check)
+
+  def date_picker_EndDate_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    if len(str(self.date_picker_EndDate.date)) == 10:
+      self.end_date_check = True
+    else:
+      self.end_date_check = False
+    print(self.end_date_check)
+
+    
 
 
     
